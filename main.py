2430bd761490b85b3955bc3a220baf120490f4ab
Tom Johnson <trjohnson3@gmail.com>
2024-06-23 15:20:21 -0500

    more setup

--- main.py
+++ main.py
@@ -9,6 +9,52 @@ WINDOW_HEIGHT = 400
 display = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Feed the Dragon')
 
+#Set FPS and clock
+FPS = 60
+clock = pygame.time.Clock()
+
+#Set game values
+PLAYER_STARTING_LIVES = 5
+VELOCITY = 5
+COIN_STARTING_VELOCITY = 5
+COIN_ACCELERATION = .5
+BUFFER_DISTANCE = 100
+
+score = 0
+player_lives = PLAYER_STARTING_LIVES
+coin_velocity = COIN_STARTING_VELOCITY
+
+#Set colors
+GREEN = (0, 255, 0)
+DARK_GREEN = (10, 50, 10)
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
+#Set fonts
+font = pygame.font.Font('./fonts/AttackGraffiti.ttf', 32)
+
+#Set text
+score_text = font.render("Score: " + str(score), True, GREEN, DARK_GREEN)
+score_rect = score_text.get_rect()
+score_rect.topleft = (10, 10)
+
+title_text = font.render("Feed the Dragon", True, GREEN, WHITE)
+title_rect = title_text.get_rect()
+title_rect.centerx = WINDOW_WIDTH//2
+title_rect.y = 10
+
+
+lives_text = font.render("Lives: " + str(player_lives), True, GREEN, DARK_GREEN)
+lives_rect = lives_text.get_rect()
+lives_rect.topright = (WINDOW_WIDTH - 10, 10)
+
+#Set sounds and music
+
+
+#Set images
+
+
+
 
 
 #Main game loop
